aaccb155ced5f7f64e2501a53bda149a2ba9fa90
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2023-05-19 12:47:28 +1000

    Update JoinDF.cs

--- PandorasBox/Features/Commands/JoinDF.cs
+++ PandorasBox/Features/Commands/JoinDF.cs
@@ -1,14 +1,20 @@
 using Dalamud.Game.ClientState.Conditions;
+using Dalamud.Utility.Signatures;
 using ECommons;
 using ECommons.Automation;
 using ECommons.DalamudServices;
 using ECommons.Throttlers;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FFXIVClientStructs.FFXIV.Client.System.Framework;
+using Lumina.Excel.GeneratedSheets;
+using Lumina.Excel;
+using PandorasBox.FeaturesSetup;
 
 namespace PandorasBox.Features.Commands
 {
@@ -20,16 +26,97 @@ namespace PandorasBox.Features.Commands
 
         public override List<string> Parameters => new List<string>() { "test", "test2", "test3" };
         public override string Description => "Activates the join button in duty finder to queue into whatever duty is selected. Optionally provide a duty as argument to select that one.";
+
+        public Configs Config { get; private set; }
+
+        public override bool UseAutoConfig => true;
+
+        public class Configs : FeatureConfig
+        {
+            [FeatureConfigOption("Fuzzy Matching Threshold", IntMin = 0, IntMax = 10, EditorSize = 300)]
+            public int fuzzyMatchingThreshold = 4;
+        }
         protected override void OnCommand(List<string> args)
         {
-            foreach (var p in Parameters)
+            // foreach (var p in Parameters)
+            // {
+            //     if (args.Any(x => x == p))
+            //     {
+            //         Svc.Chat.Print($"Test command executed with argument {p}.");
+            //         // var gatheringPoint = Svc.Data.GetExcelSheet<ContentFinderCondition>().First(x => x.RowId == duty);
+            //         SelectJoin();
+            //     }
+            // }
+
+            var arg = string.Join(" ", args);
+            List<string> allowedContentTypes = new List<string>()
             {
-                if (args.Any(x => x == p))
+                "Dungeons",
+                "Guildhests",
+                "Trials",
+                "Raids",
+                "PvP",
+                "Gold Saucer",
+                "Ultimate Raids"
+            };
+
+            var duties = Svc.Data.GetExcelSheet<ContentFinderCondition>()
+                .Where(c => allowedContentTypes.Contains(c.ContentType.ToString()))
+                .Select(c => c.Name)
+                .ToList();
+
+            List<string> fuzzyMatches = new List<string>();
+
+            foreach (string duty in duties)
+            {
+                int matchScore = CalculateFuzzyMatchScore(duty, arg);
+                if (matchScore >= Config.fuzzyMatchingThreshold)
                 {
-                    Svc.Chat.Print($"Test command executed with argument {p}.");
-                    SelectJoin();
+                    fuzzyMatches.Add(duty);
                 }
             }
+
+            if (fuzzyMatches.Count == 0)
+            {
+                Svc.Chat.Print($"Unable to match {arg} to a valid duty.");
+            }
+
+            var matchedDuty = fuzzyMatches.FirstOrDefault();
+            var cfc = Svc.Data.GetExcelSheet<ContentFinderCondition>()!
+                .FirstOrDefault(cfc => cfc.Name == matchedDuty);
+
+            if (cfc == null) return;
+
+            OpenRegularDuty(cfc.RowId); // this opens df to the selected duty, it still needs to be checked
+            SelectJoin();
+        }
+
+        [Signature("48 89 6C 24 ?? 48 89 74 24 ?? 57 48 81 EC ?? ?? ?? ?? 48 8B F9 41 0F B6 E8")]
+        private readonly OpenDutyDelegate openDuty = null!;
+
+        private delegate IntPtr OpenDutyDelegate(IntPtr agent, uint contentFinderCondition, byte a3);
+
+        // public static List<uint> GetInstanceListFromDutyName(string duty)
+        // {
+        //     return Svc.Data.GetExcelSheet<ContentFinderCondition>()!
+        //             .Where(c => c.Name == duty)
+        //             .OrderBy(c => c.SortKey)
+        //             .Select(c => c.TerritoryType.Row)
+        //             .ToList();
+        // }
+
+        private unsafe bool ShowDutyFinder(uint cfcID)
+        {
+            if (cfcID == 0)
+                return false;
+
+            var framework = Framework.Instance();
+            var uiModule = framework->GetUiModule();
+            var agentModule = uiModule->GetAgentModule();
+            var cfAgent = (IntPtr)agentModule->GetAgentByInternalId(AgentId.ContentsFinder);
+
+            this.openDuty(cfAgent, cfcID, 0);
+            return true;
         }
 
         private unsafe bool? SelectJoin()
@@ -70,6 +157,51 @@ namespace PandorasBox.Features.Commands
             {
                 return false;
             }
+        }
+
+        private static int CalculateFuzzyMatchScore(string source, string target)
+        {
+            int sourceLength = source.Length;
+            int targetLength = target.Length;
+
+            if (sourceLength == 0)
+                return targetLength;
+
+            if (targetLength == 0)
+                return sourceLength;
+
+            int[,] matrix = new int[sourceLength + 1, targetLength + 1];
+
+            for (int i = 0; i <= sourceLength; i++)
+            {
+                matrix[i, 0] = i;
+            }
+
+            for (int j = 0; j <= targetLength; j++)
+            {
+                matrix[0, j] = j;
+            }
+
+            for (int i = 1; i <= sourceLength; i++)
+            {
+                for (int j = 1; j <= targetLength; j++)
+                {
+                    int cost = (target[j - 1] == source[i - 1]) ? 0 : 1;
+
+                    int deletion = matrix[i - 1, j] + 1;
+                    int insertion = matrix[i, j - 1] + 1;
+                    int substitution = matrix[i - 1, j - 1] + cost;
+
+                    matrix[i, j] = Math.Min(Math.Min(deletion, insertion), substitution);
+                }
+            }
+
+            return matrix[sourceLength, targetLength];
+        }
+
+        private void OpenRegularDuty(uint contentFinderCondition)
+        {
+            AgentContentsFinder.Instance()->OpenRegularDuty(contentFinderCondition);
         }
     }
 }
